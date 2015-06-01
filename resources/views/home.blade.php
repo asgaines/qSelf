651d1c114e8b6148c2acdd6f1979738a88f8744f
interpopulus <andrew.s.gaines@gmail.com>
2015-06-01 14:06:26 -0600

    Fixed error on home page when no temp data exists

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,51 +8,52 @@
 
 @section('content')
 	<h1>Your Dashboard</h1>
+	@if (!$temperatures->isEmpty())
+		<div class="graph">
+			<div id="visualization"></div>
+			<script type="text/javascript">
+	    	var container = document.getElementById('visualization');
+				var groups = new vis.DataSet();
+				groups.add({
+					id: 1,
+					content: "Temperatures",
+				});
+				groups.add({
+					id: 2,
+					content: "Thoughts",
+				});
 
-	<div class="graph">
-		<div id="visualization"></div>
-		<script type="text/javascript">
-    	var container = document.getElementById('visualization');
-			var groups = new vis.DataSet();
-			groups.add({
-				id: 1,
-				content: "Temperatures",
-			});
-			groups.add({
-				id: 2,
-				content: "Thoughts",
-			});
+	  		var items = [
+					// Add each datetime of the temperature recordings as data points
+					@foreach ($temperatures as $temperature)
+						{x: '{{ $temperature->created_at }}', y: '{{ $temperature->temperature }}', group: 1, label: {content: "{{ $temperature->temperature }}"}},
+					@endforeach
 
-  		var items = [
-				// Add each datetime of the temperature recordings as data points
-				@foreach ($temperatures as $temperature)
-					{x: '{{ $temperature->created_at }}', y: '{{ $temperature->temperature }}', group: 1, label: {content: "{{ $temperature->temperature }}"}},
-				@endforeach
+					// Add each thought as a separate data point hovering above the interpolated temperature
+					@foreach ($thoughts as $thought)
+						{x: '{{ $thought->created_at }}', y: 108.5, group: 2, label: {content: "{!! $thought->thought !!}"}},
+					@endforeach
+			  ];
 
-				// Add each thought as a separate data point hovering above the interpolated temperature
-				@foreach ($thoughts as $thought)
-					{x: '{{ $thought->created_at }}', y: 108.5, group: 2, label: {content: "{!! $thought->thought !!}"}},
-				@endforeach
-		  ];
+			  var dataset = new vis.DataSet(items);
 
-		  var dataset = new vis.DataSet(items);
-
-		  var options = {
-		    start: '{{ $temperatures[0]->created_at->subDay(1) }}',
-		    end: '{{ $temperatures->last()->created_at->addDay(1) }}',
-				drawPoints: { // Circles look better
-          style: 'circle' // square, circle
-        },
-				dataAxis: {
-					// Sets the y-axis limits, showing more fluctuation in data
-	        left: {
-	          range: {min:95, max:109}
+			  var options = {
+			    start: '{{ $temperatures[0]->created_at->subDay(1) }}',
+			    end: '{{ $temperatures->last()->created_at->addDay(1) }}',
+					drawPoints: { // Circles look better
+	          style: 'circle' // square, circle
 	        },
-	    	}
-		  };
-		  var graph2d = new vis.Graph2d(container, dataset, groups, options);
-			graph2d.fit(); // Creates a neat effect, growing to a better fit from the day-off initial vals
-		</script>
+					dataAxis: {
+						// Sets the y-axis limits, showing more fluctuation in data
+		        left: {
+		          range: {min:95, max:109}
+		        },
+		    	}
+			  };
+			  var graph2d = new vis.Graph2d(container, dataset, groups, options);
+				graph2d.fit(); // Creates a neat effect, growing to a better fit from the day-off initial vals
+			</script>
+		@endif
 
 		{{-- <div id="visualization"></div>
 		<script type="text/javascript">
